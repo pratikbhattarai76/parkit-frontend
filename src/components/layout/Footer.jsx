04981810a2ee507e829dc8c5cd83f76d7c71f58d
pratikbhattarai76 <pratikbhattarai76@gmail.com>
2026-08-23 10:13:13 +0545

    fix: removed non-existent Github and Twitter lucide icons that caused white screen crash

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Car, Github, Twitter } from "lucide-react";
+import { Car } from "lucide-react";
 
 export default function Footer() {
   const year = new Date().getFullYear();
